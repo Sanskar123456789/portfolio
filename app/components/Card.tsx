0cd56c35fee676145fca9f6934e2b6df05c93b3d
Sanskar Agarwal <sanskar.agarwal91@gmail.com>
2024-02-06 00:20:01 +0530

    Enhancements 1

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -9,14 +9,14 @@ interface Props {
 
 const Card: NextPage<Props> = ({ image, docUrl }) => {
   return (
-    <div className=" shadow-lg h-52 w-52 hover:scale-110 transition rounded-full">
+    <div className="border border-gray-300 shadow-lg h-52 rounded-full hover:scale-110 transition bg-white">
       <Link href={docUrl} target="_blank">
         <Image
           src={image}
           alt="no image"
           width="0"
           height="0"
-          className="w-full h-full rounded-full"
+          className="w-full h-full"
           unoptimized
         ></Image>
       </Link>
